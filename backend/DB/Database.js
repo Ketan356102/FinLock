3e9563e332becfb0afa47cb44344d6c3f1cef64d
Ketan356102 <ketansuthar493@gmail.com>
2026-06-04 16:23:14 +0530

    FinLock

---
+++ backend/DB/Database.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export const connectDB= async (req,res) => {
+    const db = process.env.MONGO_URL;
+    const { connection } = await mongoose.connect(db);
+    console.log("MongoDB Connected Successfully");
+}
---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./DB/Database.js";
+import userRouter from "./router/user-router.js";
+import otpRouter from "./router/otp-router.js";
+import transactionRouter from "./router/transaction-router.js";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    })
+);
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (request, response) => {
+    response.send("Hello World")
+})
+
+app.use("/api/user", userRouter);
+app.use("/api/otp", otpRouter);
+app.use("/api/transaction", transactionRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
